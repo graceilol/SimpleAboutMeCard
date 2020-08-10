b41cf5713b47755eae306b20831aa5df79b1ee63
graceilol <gigehappy123@gmail.com>
2020-08-10 10:13:18 -0500

    finished the card and styled the card componets very easy and simple

---
+++ app.js
@@ -0,0 +1,14 @@
+//ABOUT ME PROJECT USING PLAYING JS AND CSS --yes i know is not a good practice but is just to know how the dom is manipulated--
+let aboutMe = document.write(`
+
+<body>
+<div class="card">
+<div class="about-me">
+   <h1>About Me</h1>
+   <p>I am a young developer in training, learning how to code using HTML, CSS and JS. In the begining it was very chalenging, trying to figure out how html tags and css properties works is not an easy task. Then i started practicing what i learned, developing small projects and watching other people do the same thing. It was very helpfull and i thank all the teachers that helped me get to the level that i am know. </p>
+   <a href="#">Learn More</a>
+</div>
+</div>
+</body>
+
+`);
